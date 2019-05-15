d13dfc70a1fec154225e793c1a1abff39ff5da4e
autinmitra <autin123@gmail.com>
2019-05-15 11:56:45 -0400

    Navbar setup (Kinda)

--- mugen/src/app/app.module.ts
+++ mugen/src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { 
+  MdcIconModule,
+  MdcTypographyModule,
+  MdcButtonModule,
+  MdcTopAppBarModule
+} from '@angular-mdc/web';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdcIconModule,
+    MdcTypographyModule,
+    MdcButtonModule,
+    MdcTopAppBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
