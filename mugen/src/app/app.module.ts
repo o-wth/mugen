aaf578d38305d9dc37b083739ab9bf16fc59172f
Autin Mitra <autinmitra@pop-os.localdomain>
2019-05-17 11:38:47 -0400

    Created initial UI mockup

--- mugen/src/app/app.module.ts
+++ mugen/src/app/app.module.ts
@@ -2,24 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './views/player/player.component';
 
 import { 
   MdcIconModule,
   MdcTypographyModule,
   MdcButtonModule,
-  MdcTopAppBarModule
+  MdcTopAppBarModule,
+  MdcElevationModule,
+  MdcFabModule,
+  MdcSelectModule,
+  MdcListModule,
 } from '@angular-mdc/web';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
     MdcIconModule,
     MdcTypographyModule,
     MdcButtonModule,
-    MdcTopAppBarModule
+    MdcTopAppBarModule,
+    MdcElevationModule,
+    MdcFabModule,
+    MdcSelectModule,
+    MdcListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
