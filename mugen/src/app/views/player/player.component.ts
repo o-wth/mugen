0f74257d97260ccb1dc403d85cb440b996ab97f5
sumanthratna <sumanthratna@gmail.com>
2019-05-24 14:46:03 -0400

    add event listeners

--- mugen/src/app/views/player/player.component.ts
+++ mugen/src/app/views/player/player.component.ts
@@ -10,13 +10,30 @@ export class PlayerComponent implements OnInit {
 
   musicTypeForm: FormGroup;
 
-  constructor() { 
+  constructor() {
     this.musicTypeForm = new FormGroup({
       musicType: new FormControl('Classical')
     })
   }
 
+  rewindFABAction() {
+    console.log('rewind');
+  }
+  controlFABAction(element: HTMLElement) {
+    element.setAttribute('icon', 'pause');
+    // won't work because HTML and angular-mdc-web are different
+    console.log(element.getAttribute('icon'))
+  }
+  forwardFABAction() {
+    console.log('forward');
+  }
   ngOnInit() {
+    const rewindFAB: HTMLElement = document.getElementById('rewind');
+    rewindFAB.addEventListener('click', (e: Event) => this.rewindFABAction());
+    const controlFAB: HTMLElement = document.getElementById('control');
+    controlFAB.addEventListener('click', (e: Event) => this.controlFABAction(controlFAB));
+    const forwardFAB: HTMLElement = document.getElementById('forward');
+    forwardFAB.addEventListener('click', (e: Event) => this.forwardFABAction());
   }
 
 }
