ef19cbbe6a60bc06c81cde652cae51f55ad52f60
sumanthratna <sumanthratna@gmail.com>
2019-05-29 19:18:07 -0400

    Add hyperlinks

--- mugen/src/app/views/player/player.component.ts
+++ mugen/src/app/views/player/player.component.ts
@@ -1,33 +1,49 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+import * as mm from '@magenta/music';
+
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-
-  musicTypeForm: FormGroup;
+  const classical = new mm.MusicVAE('https://github.com/o-wth/mugen/raw/master/data/classical/attention_rnn.mag');
+  const player = new mm.Player();
 
   constructor() {
-    this.musicTypeForm = new FormGroup({
-      musicType: new FormControl('Classical')
-    })
+
   }
 
-  rewindFABAction() {
+  play(genre: string) {
+    switch (genre) {
+      case 'classical': {
+        break;
+      }
+      case 'country': {
+        break;
+      }
+      case 'rap': {
+        break;
+      }
+      default: {
+        console.error(`MUGEN ERROR: genre not found (received: ${string})`);
+        break;
+      }
+    }
+  }
+  pause() {
+    this.player.pause();
+  }
+  forward() {
+    console.log('fast forward');
+  }
+  backward() {
     console.log('rewind');
   }
-  controlFABAction(element: HTMLElement) {
-    element.setAttribute('icon', 'pause');
-    // won't work because HTML and angular-mdc-web are different
-    console.log(element.getAttribute('icon'))
-  }
-  forwardFABAction() {
-    console.log('forward');
-  }
   ngOnInit() {
+    this.classical.initialize();
   }
 
 }
